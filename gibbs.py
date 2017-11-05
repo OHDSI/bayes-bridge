78c585d22eb89d8476d89cc17317ee0e04af616e
Aki Nishimura <akihiko_nishimura@hotmail.com>
2017-11-04 18:22:16 -0700

    Cleand the code: refactoring and updating comments.

--- gibbs.py
+++ gibbs.py
@@ -5,38 +5,34 @@ import math
 import warnings
 
 
-def gibbs(y, X, n_burnin, n_post_burnin, thin, fixed_tau, tau, lam0=None):
+def gibbs(y, X, n_burnin, n_post_burnin, thin, fixed_tau=False,
+          tau = None, lam0=None):
     """
-    Function to impelement Horseshoe shrinkage prior (http://faculty.chicagobooth.edu/nicholas.polson/research/papers/Horse.pdf)
-    in Bayesian Linear Regression.
-    Based on code by James Johndrow (johndrow@stanford.edu)
+    MCMC implementation for Bayesian linear regression with the horseshoe prior.
+    Based on code by Antik Chakraborty, Anirban Bhattacharya, and James Johndrow
     Modified and ported to Python by Akihiko Nishimura
 
     Model: y = X\beta + \epslion, \epsilon \sim N(0, \sigma^2) 
            \beta_j \sim N(0, \sigma^2 \lambda_j^2 \tau^2) 
-           \lambda_j \sim Half-Cauchy(0, 1), \tau \sim Half-Cauchy (0, 1) 
+           \lambda_j \sim Half-Cauchy(0, 1),
+           \tau \sim Half-Cauchy (0, 1), 
            \pi(\sigma^2) \sim 1/\sigma^2 
 
 
-    This function implements the algorithm proposed in "Scalable MCMC for
-    Bayes Shrinkage Priors" by Johndrow and Orenstein (2017).
-    The global local scale parameters are updated via a Slice sampling scheme given in the online supplement
-    of "The Bayesian Bridge" by Polson et. al. (2011). Setting ab = true
-    implements the algorithm of Bhattacharya et al. Setting ab = false
-    implements the algorith of Johndrow and Orenstein, which uses a block
-    update for \tau, \sigma^2, \beta
+    This function implements the algorithm proposed in "Scalable MCMC for Bayes
+    Shrinkage Priors" by Johndrow and Orenstein (2017). The local scale
+    parameters are updated via a slice sampling scheme given in the online
+    supplement of "The Bayesian Bridge" by Polson et. al. (2011).
 
-    Input: y = response, a n * 1 vector 
-           X = matrix of covariates, dimension n * p 
-           n_burnin = number of burnin MCMC samples 
-           n_post_burnin = number of posterior draws to be saved 
-           thin = thinning parameter of the chain 
-           and lower bound on MH proposals usually make this 1 and just make
-           scl_ub = scl_lb only use for particularly challenging cases
+    Input: y = response, a n * 1 vector
+           X = matrix of covariates, dimension n * p
+           n_burnin = number of burnin MCMC samples
+           n_post_burnin = number of posterior draws to be saved
+           thin = thinning parameter of the chain
            fixed_tau = if true, tau will not be updated.
            lam0 = the initial value for MCMC
     """
-    
+
 
     n_iter = n_burnin + n_post_burnin
     n_sample = math.ceil(n_post_burnin / thin)  # Number of samples to keep
@@ -46,8 +42,8 @@ def gibbs(y, X, n_burnin, n_post_burnin, thin, fixed_tau, tau, lam0=None):
     a0 = .5
     b0 = .5
 
-    # Stepsize of Metropolis. Apparently, .8 is a good default (Johndrow and Orenstein 2017).
-    std_MH = .8
+    # The value proposed as a good default in Johndrow and Orenstein (2017).
+    metropolis_sd = .8
 
     # Initial state of the Markov chain
     beta = np.zeros(p)  # Unused with the current gibbs update order.
@@ -66,20 +62,20 @@ def gibbs(y, X, n_burnin, n_post_burnin, thin, fixed_tau, tau, lam0=None):
     lam_samples = np.zeros((p, n_sample))
     tau_samples = np.zeros(n_sample)
     sigma_sq_samples = np.zeros(n_sample)
-    accept_prob = np.zeros(n_post_burnin + n_burnin)
+    accepted = np.zeros(n_post_burnin + n_burnin)
     I_n = np.eye(n)
 
-    # start Gibbs sampling #
+    # Start Gibbs sampling
     for i in range(n_iter):
 
         LX = lam[:, np.newaxis] ** 2 * X.T
         XLX = np.dot(X, LX)
 
-        # update tau
+        # Update tau
         M = I_n + (1 / xi) * XLX
         M_chol = sp.linalg.cholesky(M)
         if not fixed_tau:
-            prop_xi = xi * math.exp(std_MH * np.random.randn())
+            prop_xi = xi * math.exp(metropolis_sd * np.random.randn())
             try:
                 M_chol_prop = sp.linalg.cholesky(I_n + (1 / prop_xi) * XLX)
             except:
@@ -87,19 +83,18 @@ def gibbs(y, X, n_burnin, n_post_burnin, thin, fixed_tau, tau, lam0=None):
                     'Proposal rejected because of a non-positive-definite matrix.',
                     RuntimeWarning)
                 break
-            lrat_curr = lmh_ratio(M_chol, y, xi, n, a0, b0)
-            lrat_prop = lmh_ratio(M_chol_prop, y, prop_xi, n, a0, b0)
-            log_acc_rat = (lrat_prop - lrat_curr) + (
-            math.log(prop_xi) - math.log(xi))
+            logp_curr = compute_beta_logp(M_chol, y, xi, a0, b0)
+            logp_prop = compute_beta_logp(M_chol_prop, y, prop_xi, a0, b0)
 
-            accept_prob[i] = min(1, math.exp(log_acc_rat))
-            if accept_prob[i] > np.random.rand():  # if accepted, update
+            accepted[i] = np.random.rand() < \
+                math.exp(logp_prop - logp_curr + math.log(prop_xi) - math.log(xi))
+            if accepted[i]:
                 xi = prop_xi
                 M_chol = M_chol_prop
 
         tau = 1 / math.sqrt(xi)
 
-        # update sigma_sq marginal of beta #
+        # Update sigma_sq with beta maginalized out
         xtmp = sp.linalg.cho_solve((M_chol, False), y)
         ssr = np.dot(y, xtmp)
         sigma_sq = 1 / np.random.gamma((n + a0) / 2, 2 / (ssr + b0))
@@ -116,12 +111,12 @@ def gibbs(y, X, n_burnin, n_post_burnin, thin, fixed_tau, tau, lam0=None):
         sigma_sq = 1 / gamrnd((n + p + a0) / 2, 2 / (b0 + E_1 + E_2))
         """
 
-        # update beta #
+        # Update beta
         D = (tau * lam) ** 2
         beta = math.sqrt(sigma_sq) \
                * generate_gaussian(y / math.sqrt(sigma_sq), X, D, M_chol, True)
 
-        # update lam_j's in a block using slice sampling #
+        # Update lam_j's using slice sampling
         u = np.random.rand(p) / (eta + 1)
         gamma_rate = (beta ** 2) * xi / (2 * sigma_sq)
         upper_bd = (1 - u) / u
@@ -157,20 +152,15 @@ def gibbs(y, X, n_burnin, n_post_burnin, thin, fixed_tau, tau, lam0=None):
     return beta_samples, sigma_sq_samples, lam_samples, tau_samples
 
 
-def lmh_ratio(M_chol, y, xi, n, a0, b0):
+def compute_beta_logp(M_chol, y, xi, a0, b0):
     """ Computes the log posterior conditional of 'beta' given 'lam'. """
-    try:
-        x = sp.linalg.cho_solve((M_chol, False), y)
-        ssr = np.dot(y, x) + b0
-        ldetM = 2 * np.sum(np.log(np.diag(M_chol)))
-        loglik = - .5 * ldetM - ((n + a0) / 2) * math.log(ssr)
-        log_prior = - np.log(math.sqrt(xi) * (1 + xi))
-        logp = loglik + log_prior
-    except:
-        logp = - np.float('inf')
-        warnings.warn(
-            'Proposal rejected because I + XDX was not positive-definite.',
-            RuntimeWarning)
+    n = len(y)
+    x = sp.linalg.cho_solve((M_chol, False), y)
+    ssr = np.dot(y, x) + b0
+    ldetM = 2 * np.sum(np.log(np.diag(M_chol)))
+    loglik = - .5 * ldetM - ((n + a0) / 2) * math.log(ssr)
+    log_prior = - np.log(math.sqrt(xi) * (1 + xi))
+    logp = loglik + log_prior
 
     return logp
 
