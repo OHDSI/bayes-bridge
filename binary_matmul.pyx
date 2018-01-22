7386147e6c8610b21736e5a0c86a7896015d84cd
Aki Nishimura <akihiko4@g.ucla.edu>
2018-01-22 14:35:32 -0800

    Separated the python wrapper function and the cythonized function.

--- binary_matmul.pyx
+++ binary_matmul.pyx
@@ -7,18 +7,19 @@ ctypedef np.int32_t INT_t
 ctypedef np.float_t FLOAT_t
 FLOAT = np.float64
 
-#cpdef binary_matmul(np.ndarray[INT_t, ndim=1] indices, np.ndarray[INT_t, ndim=1] indptr, np.ndarray[FLOAT_t, ndim=1] v):
+def binary_matmul(X_csr, v):
+  return c_binary_matmul(X_csr.indices, X_csr.indptr, v)
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cdef binary_matmul(np.ndarray[INT_t, ndim=1] indices, np.ndarray[INT_t, ndim=1] indptr, np.ndarray[FLOAT_t, ndim=1] v):
+cdef c_binary_matmul(np.ndarray[INT_t, ndim=1] indices, np.ndarray[INT_t, ndim=1] indptr, np.ndarray[FLOAT_t, ndim=1] v):
     cdef int i, k
     cdef int m = indptr.shape[0] - 1
     cdef FLOAT_t val
     cdef np.ndarray[FLOAT_t, ndim=1] Xv = np.zeros(m, dtype=FLOAT)
-    for i in range(m, nogil=True):
-      val = 0
-      for k in range(indptr[i], indptr[i + 1]):
-        val += v[indices[k]]
-      Xv[i] = val
+    for i in range(m):
+        val = 0
+        for k in range(indptr[i], indptr[i + 1]):
+            val += v[indices[k]]
+        Xv[i] = val
     return Xv
