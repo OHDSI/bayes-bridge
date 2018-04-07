7984de9b36d63bdc4c85b216814f962e0974d94e
Aki Nishimura <akihiko_nishimura@hotmail.com>
2018-04-07 16:56:24 -0700

    Added 'periods' to numbers to make them 'float' explicitly: not necessary in Python, but just for consistency.

--- tilted_stable_dist/rand_exp_tilted_stable.py
+++ tilted_stable_dist/rand_exp_tilted_stable.py
@@ -21,12 +21,11 @@ class ExpTiltedStableDist():
         (The density p(x) is tilted by exp(-lam * x).)
         """
 
-
-        b = (1 - alpha) / alpha
+        b = (1. - alpha) / alpha
         lam_alpha = lam ** alpha
-        gamma = lam_alpha * alpha * (1 - alpha)
+        gamma = lam_alpha * alpha * (1. - alpha)
         sqrt_gamma = sqrt(gamma)
-        c1 = sqrt(math.pi / 2)
+        c1 = sqrt(math.pi / 2.)
         c2 = 2. + c1
         c3 = c2 * sqrt_gamma
         xi = (1. + sqrt(2.) * c3) / math.pi
@@ -43,7 +42,7 @@ class ExpTiltedStableDist():
                 V1 = self.unif_rv()
                 if gamma >= 1:
                     if V1 < w1 / (w1 + w2):
-                        U = abs(self.normal_rv(0, 1)) / sqrt_gamma
+                        U = abs(self.normal_rv(0., 1.)) / sqrt_gamma
                     else:
                         W1 = self.unif_rv()
                         U = math.pi * (1. - W1 * W1)
@@ -55,21 +54,21 @@ class ExpTiltedStableDist():
                         U = math.pi * (1. - W1 * W1)
                 W2 = self.unif_rv()
                 zeta = sqrt(self.BdB0(U, alpha))
-                z = 1 / (1. - pow(1 + alpha * zeta / sqrt_gamma, -1 / alpha))
+                z = 1. / (1. - pow(1. + alpha * zeta / sqrt_gamma, -1 / alpha))
                 rho = math.pi * exp(-lam_alpha * (1. - 1. / (zeta * zeta))) \
                     / ((1. + c1) * sqrt_gamma / zeta + z)
                 d = 0.
-                if U >= 0 and gamma >= 1:
+                if U >= 0. and gamma >= 1:
                     d += xi * exp(-gamma * U * U / 2.)
-                if U > 0 and U < math.pi:
+                if U > 0. and U < math.pi:
                     d += psi / sqrt(math.pi - U)
-                if U >= 0 and U <= math.pi and gamma < 1:
+                if U >= 0. and U <= math.pi and gamma < 1.:
                     d += xi
                 rho *= d
                 Z = W2 * rho
                 aug_accepted = (U < math.pi and Z <= 1.)
 
-            a = pow(self.A_3(U, alpha), 1. / (1 - alpha))
+            a = pow(self.A_3(U, alpha), 1. / (1. - alpha))
             m = pow(b / a, alpha) * lam_alpha
             delta = sqrt(m * alpha / a)
             a1 = delta * c1
@@ -79,7 +78,7 @@ class ExpTiltedStableDist():
             N = 0.
             E1 = 0.
             if V2 < a1 / s:
-                N = self.normal_rv(0, 1)
+                N = self.normal_rv(0., 1.)
                 X = m - delta * abs(N)
             else:
                 if V2 < (a1 + delta) / s:
@@ -89,19 +88,19 @@ class ExpTiltedStableDist():
                     X = m + delta + E1 * a3
             if X > 0:
                 E2 = -log(Z)
-                c = a * (X - m) + exp((1 / alpha) * log(lam_alpha) - b * log(m)) * (pow(m / X, b) - 1)
+                c = a * (X - m) + exp((1. / alpha) * log(lam_alpha) - b * log(m)) * (pow(m / X, b) - 1.)
                     #/**< Marius Hofert: numerically more stable for small alpha */
                 if X < m:
                     c -= N * N / 2.
                 elif X > m + delta:
                     c -= E1
-            accepted = (X >= 0 and c <= E2)
+            accepted = (X >= 0. and c <= E2)
 
         return pow(X, -b)
 
     def BdB0(self, x, alpha):
         denominator = pow(self.sinc(alpha * x), alpha) \
-                      * pow(self.sinc((1 - alpha) * x), (1 - alpha))
+                      * pow(self.sinc((1. - alpha) * x), (1. - alpha))
         numerator = self.sinc(x)
         return numerator / denominator
 
@@ -112,8 +111,8 @@ class ExpTiltedStableDist():
     def sinc(self, x):
         ax = abs(x)
         if ax < 0.006:
-            if x == 0:
-                return 1
+            if x == 0.:
+                return 1.
             x2 = x * x
             if ax < 2e-4:
                  return 1. - x2 / 6.
