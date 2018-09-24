aa680735c2ab744e6bc265dec65fe0ef317f2530
Aki Nishimura <akihiko_nishimura@hotmail.com>
2018-09-24 11:33:07 -0700

    Add a setup file to make the package instalable.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bayesbridge',
+    version='0.1',
+    description=\
+        'Generates posterior samples under Bayesian sparse regression based on '
+        + 'the bridge prior using the CG-accelerated Gibbs sampler of Nishimura '
+        + 'et. al. (2018). The linear and logistic model are currently supported.',
+    url='TBD',
+    author='Akihiko (Aki) Nishimura',
+    author_email='akihiko4@g.ucla.edu',
+    license='MIT',
+    packages=find_packages(exclude=['./tests/*']),
+    install_requires=[
+        'numpy', 'scipy', 'pypolyagamma'
+    ],
+    zip_safe=False
+)
