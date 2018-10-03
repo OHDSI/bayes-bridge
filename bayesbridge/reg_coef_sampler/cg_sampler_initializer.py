7182eb70e921e9380437b114b2bd27939e04b4a4
Aki Nishimura <akihiko_nishimura@hotmail.com>
2018-10-02 21:07:57 -0700

    Rename the variables for the local and global shrinkage parameters.

--- bayesbridge/reg_coef_sampler/cg_sampler_initializer.py
+++ bayesbridge/reg_coef_sampler/cg_sampler_initializer.py
@@ -3,23 +3,23 @@ from ..util.onthefly_summarizer import OntheflySummarizer
 
 class CgSamplerInitializer():
 
-    def __init__(self, beta, tau, lam):
-        self.n_unshrunk = len(beta) - len(lam)
-        beta_scaled = self.scale_beta(beta, tau, lam)
+    def __init__(self, beta, gshrink, lshrunk):
+        self.n_unshrunk = len(beta) - len(lshrunk)
+        beta_scaled = self.scale_beta(beta, gshrink, lshrunk)
         self.beta_scaled_summarizer = OntheflySummarizer(beta_scaled)
 
-    def scale_beta(self, beta, tau, lam):
+    def scale_beta(self, beta, gshrunk, lshrunk):
         beta_scaled = beta.copy()
-        beta_scaled[self.n_unshrunk:] /= tau * lam
+        beta_scaled[self.n_unshrunk:] /= gshrunk * lshrunk
         return beta_scaled
 
-    def update(self, beta, tau, lam):
-        beta_scaled = self.scale_beta(beta, tau, lam)
+    def update(self, beta, gshrunk, lshrunk):
+        beta_scaled = self.scale_beta(beta, gshrunk, lshrunk)
         self.beta_scaled_summarizer.update_stats(beta_scaled)
 
-    def guess_beta_condmean(self, tau, lam):
+    def guess_beta_condmean(self, gshrunk, lshrunk):
         beta_condmean_guess = self.beta_scaled_summarizer.stats['mean'].copy()
-        beta_condmean_guess[self.n_unshrunk:] *= tau * lam
+        beta_condmean_guess[self.n_unshrunk:] *= gshrunk * lshrunk
         return beta_condmean_guess
 
     def estimate_beta_precond_scale_sd(self):
