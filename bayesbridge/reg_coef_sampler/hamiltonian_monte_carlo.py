67a1c37f2076adef7aba7132f2fb59802c7752db
Aki Nishimura <akihiko4@g.ucla.edu>
2018-10-08 11:57:01 -0700

    Modify the numpy arrays in place for better memory efficiency.

--- bayesbridge/reg_coef_sampler/hamiltonian_monte_carlo.py
+++ bayesbridge/reg_coef_sampler/hamiltonian_monte_carlo.py
@@ -88,7 +88,8 @@ def generate_next_state(f, dt, n_step, theta0, logp0=None, grad0=None):
 def simulate_dynamics(f, dt, n_step, theta0, p0, grad0):
 
     n_grad_evals = 0
-    theta, p, grad = theta0.copy(), p0.copy(), grad0.copy()
+    theta, grad = theta0.copy(), grad0.copy()
+    p = p0 # Modify in place without copying.
     theta, p, grad, logp \
         = integrator(f, dt, theta, p, grad)
     n_grad_evals += 1
@@ -101,13 +102,13 @@ def simulate_dynamics(f, dt, n_step, theta0, p0, grad0):
 
     return theta, p, grad, logp, n_grad_evals
 
-
+# Note: the integrator modifies 'theta' and 'p' in place!
 def integrator(f, dt, theta, p, grad):
 
-    p = p + 0.5 * dt * grad
-    theta = theta + dt * p
+    p += 0.5 * dt * grad
+    theta += dt * p
     logp, grad = f(theta)
-    p = p + 0.5 * dt * grad
+    p += 0.5 * dt * grad
 
     return theta, p, grad, logp
 
