1529f8eddd2dca2f90a17f0c355af3ea3e9bb29e
Aki Nishimura <akihiko4@g.ucla.edu>
2018-11-10 15:48:48 -0800

    Split the tests of HMC to the automatic & manual regression parts.

--- tests/test_hmc.py
+++ tests/test_hmc.py
@@ -7,45 +7,6 @@ import bayesbridge.reg_coef_sampler.hamiltonian_monte_carlo as hmc
 from tests.distributions import BivariateGaussian
 
 
-
-""" Regression test. """
-
-data_folder = 'regression_test_saved_outputs/'
-test_samples_file = data_folder + 'hmc_bivariate_gaussian_samples.npy'
-
-
-def test_hmc():
-    samples = run_hmc()
-    prev_output = np.load(test_samples_file)
-    assert np.allclose(samples[:, -1], prev_output, atol=1e-10, rtol=1e-10)
-
-
-def run_hmc():
-
-    dt = np.array([.55, .63])
-    n_step = np.array([3, 4])
-    n_burnin = 10
-    n_sample = 100
-
-    theta0 = np.zeros(2)
-    f = BivariateGaussian().compute_logp_and_gradient
-
-    samples = hmc.generate_samples(
-        f, theta0, dt, n_step, n_burnin, n_sample, seed=0)[0]
-
-    return samples
-
-
-# Update the saved HMC outputs, if called as a script with option 'update'.
-if __name__ == '__main__':
-    option = sys.argv[-1]
-    if option == 'update':
-        samples = run_hmc()
-        np.save(test_samples_file, samples[:, -1])
-
-
-""" Other tests. """
-
 def test_early_termination_symmetry():
 
     bi_gauss = BivariateGaussian(rho=.9, sigma=np.array([1., 2.]))
