9c014324a76bef3a46945704ae021dc2ecf637de
Aki Nishimura <akihiko4@g.ucla.edu>
2018-11-17 12:23:52 -0800

    Test for checking gradients from synthetic distributions.

---
+++ tests/test_distributions.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from .derivative_tester import numerical_grad_is_close
+from .distributions import BivariateGaussian, BivariateSkewNormal
+
+def test_bi_gaussian():
+    x = np.ones(2)
+    skewnorm = BivariateGaussian()
+    f = skewnorm.compute_logp_and_gradient
+    assert numerical_grad_is_close(f, x)
+
+def test_skew_normal():
+    x = np.ones(2)
+    skewnorm = BivariateSkewNormal()
+    f = skewnorm.compute_logp_and_gradient
+    assert numerical_grad_is_close(f, x)
