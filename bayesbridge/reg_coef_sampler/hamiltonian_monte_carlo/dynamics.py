d24d8a1da116ef2b51cc3e7f5cb3e7c9f59ec6af
Aki Nishimura <akihiko4@g.ucla.edu>
2019-01-04 10:14:18 -0800

    Incorporate the changes in the HMC submodule.

---
+++ bayesbridge/reg_coef_sampler/hamiltonian_monte_carlo/dynamics.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+
+"""
+Defines a (numerical) Hamiltonian dynamics based on a Gaussian momentum and the 
+velocity Verlet integrator. The code is written so that other integrators & 
+momentum distributions can also be employed straightwardly.
+"""
+
+class HamiltonianDynamics():
+
+    def __init__(self, mass=None):
+        """
+        Parameters
+        ----------
+        mass(p, power) : callable
+            Returns a vector obtained by multiplying the vector p with matrix
+            M ** power for power == -1 or power == 1/2. The matrix L corresponding
+            to M ** 1/2 only needs to satisfy L L' = M. Passing M = None defaults
+            to a dynamics with the identity mass matrix.
+        """
+        self.integrator = velocity_verlet
+        self.momentum = GaussianMomentum(mass)
+
+    def integrate(self, f, dt, q, p, grad):
+        q, p, logp, grad \
+            = velocity_verlet(f, self.momentum.get_grad, dt, q, p, grad)
+        return q, p, logp, grad
+
+    def draw_momentum(self, n_param):
+        return self.momentum.draw_random(n_param)
+
+    def compute_hamiltonian(self, logp, p):
+        potential = - logp
+        kinetic = - self.momentum.get_logp(p)
+        return potential + kinetic
+
+
+def velocity_verlet(
+        get_position_logp_and_grad, get_momentum_grad, dt, q, p, position_grad
+    ):
+    p = p + 0.5 * dt * position_grad
+    q = q - dt * get_momentum_grad(p)
+    position_logp, position_grad = get_position_logp_and_grad(q)
+    p = p + 0.5 * dt * position_grad
+    return q, p, position_logp, position_grad
+
+
+class GaussianMomentum():
+
+    def __init__(self, mass=None):
+        if mass is None:
+            mass = lambda p, power: p
+        self.mass = mass
+
+    def draw_random(self, n_param):
+        p = self.mass(np.random.randn(n_param), 1/2)
+        return p
+
+    def get_grad(self, p):
+        return - self.mass(p, -1)
+
+    def get_logp(self, p):
+        return - 0.5 * np.dot(p, self.mass(p, -1))
--- bayesbridge/reg_coef_sampler/hamiltonian_monte_carlo/hmc.py
+++ bayesbridge/reg_coef_sampler/hamiltonian_monte_carlo/hmc.py
@@ -3,10 +3,17 @@ import math
 import time
 from .stepsize_adapter import HmcStepsizeAdapter
 from .util import warn_message_only
+from .dynamics import HamiltonianDynamics
+
+
+dynamics = HamiltonianDynamics()
+integrator = dynamics.integrate
+compute_hamiltonian = dynamics.compute_hamiltonian
+draw_momentum = dynamics.draw_momentum
 
 
 def generate_samples(
-        f, theta0, dt_range, nstep_range, n_burnin, n_sample,
+        f, q0, dt_range, nstep_range, n_burnin, n_sample,
         seed=None, n_update=0, adapt_stepsize=False, target_accept_prob=.9,
         final_adaptsize=.05):
     """ Run HMC and return samples and some additional info. """
@@ -24,25 +31,25 @@ def generate_samples(
         reference_iteration=n_burnin, adaptsize_at_reference=final_adaptsize
     )
 
-    theta = theta0
+    q = q0
     if n_update > 0:
         n_per_update = math.ceil((n_burnin + n_sample) / n_update)
     else:
         n_per_update = float('inf')
-    pathlen_ave = 0
-    samples = np.zeros((len(theta), n_sample + n_burnin))
+
+    samples = np.zeros((len(q), n_sample + n_burnin))
     logp_samples = np.zeros(n_sample + n_burnin)
     accept_prob = np.zeros(n_sample + n_burnin)
 
     tic = time.time()  # Start clock
-    logp, grad = f(theta)
+    logp, grad = f(q)
     use_averaged_stepsize = False
     for i in range(n_sample + n_burnin):
         dt = np.random.uniform(dt_range[0], dt_range[1])
         dt *= max_stepsize_adapter.get_current_stepsize(use_averaged_stepsize)
         nstep = np.random.randint(nstep_range[0], nstep_range[1] + 1)
-        theta, info = generate_next_state(
-            f, dt, nstep, theta, logp0=logp, grad0=grad
+        q, info = generate_next_state(
+            f, dt, nstep, q, logp0=logp, grad0=grad
         )
         logp, grad, pathlen, accept_prob[i] = (
             info[key] for key in ['logp', 'grad', 'n_grad_evals', 'accept_prob']
@@ -51,8 +58,7 @@ def generate_samples(
             max_stepsize_adapter.adapt_stepsize(info['hamiltonian_error'])
         elif i == n_burnin - 1:
             use_averaged_stepsize = True
-        pathlen_ave = i / (i + 1) * pathlen_ave + 1 / (i + 1) * pathlen
-        samples[:, i] = theta
+        samples[:, i] = q
         logp_samples[i] = logp
         if (i + 1) % n_per_update == 0:
             print('{:d} iterations have been completed.'.format(i + 1))
@@ -64,22 +70,22 @@ def generate_samples(
 
 
 def generate_next_state(
-        f, dt, n_step, theta0,
+        f, dt, n_step, q0,
         p0=None, logp0=None, grad0=None, hamiltonian_tol=100.):
 
     n_grad_evals = 0
 
     if (logp0 is None) or (grad0 is None):
-        logp0, grad0 = f(theta0)
+        logp0, grad0 = f(q0)
         n_grad_evals += 1
 
     if p0 is None:
-        p0 = draw_momentum(len(theta0))
+        p0 = draw_momentum(len(q0))
 
     log_joint0 = - compute_hamiltonian(logp0, p0)
 
-    theta, p, logp, grad, simulation_info = simulate_dynamics(
-        f, dt, n_step, theta0, p0, logp0, grad0, hamiltonian_tol
+    q, p, logp, grad, simulation_info = simulate_dynamics(
+        f, dt, n_step, q0, p0, logp0, grad0, hamiltonian_tol
     )
     n_grad_evals += simulation_info['n_grad_evals']
     instability_detected = simulation_info['instability_detected']
@@ -94,7 +100,7 @@ def generate_next_state(
 
     accepted = acceptprob > np.random.rand()
     if not accepted:
-        theta = theta0
+        q = q0
         logp = logp0
         grad = grad0
 
@@ -108,10 +114,10 @@ def generate_next_state(
         'n_grad_evals': n_grad_evals
     }
 
-    return theta, info
+    return q, info
 
 
-def simulate_dynamics(f, dt, n_step, theta0, p0, logp0, grad0, hamiltonian_tol):
+def simulate_dynamics(f, dt, n_step, q0, p0, logp0, grad0, hamiltonian_tol):
 
     n_grad_evals = 0
     instability_detected = False
@@ -125,10 +131,10 @@ def simulate_dynamics(f, dt, n_step, theta0, p0, logp0, grad0, hamiltonian_tol):
     # First integration step.
     if n_step == 0:
         warn_message_only("The number of integration steps was set to be 0.")
-        theta, p, logp, grad = theta0, p0, logp0, grad0
+        q, p, logp, grad = q0, p0, logp0, grad0
     else:
-        theta, p, logp, grad \
-            = integrator(f, dt, theta0, p0, grad0)
+        q, p, logp, grad \
+            = integrator(f, dt, q0, p0, grad0)
         hamiltonian = compute_hamiltonian(logp, p)
         hamiltonians[1] = hamiltonian
         min_h, max_h = update_running_minmax(min_h, max_h, hamiltonian)
@@ -138,8 +144,8 @@ def simulate_dynamics(f, dt, n_step, theta0, p0, logp0, grad0, hamiltonian_tol):
 
     for i in range(1, n_step):
 
-        theta, p, logp, grad \
-            = integrator(f, dt, theta, p, grad)
+        q, p, logp, grad \
+            = integrator(f, dt, q, p, grad)
         hamiltonian = compute_hamiltonian(logp, p)
         hamiltonians[i + 1] = hamiltonian
         min_h, max_h = update_running_minmax(min_h, max_h, hamiltonian)
@@ -155,28 +161,10 @@ def simulate_dynamics(f, dt, n_step, theta0, p0, logp0, grad0, hamiltonian_tol):
         'instability_detected': instability_detected,
     }
 
-    return theta, p, logp, grad, info
+    return q, p, logp, grad, info
 
 
 def update_running_minmax(running_min, running_max, curr_val):
     running_min = min(running_min, curr_val)
     running_max = max(running_max, curr_val)
     return running_min, running_max
-
-
-def integrator(f, dt, theta, p, grad):
-
-    p = p + 0.5 * dt * grad
-    theta = theta + dt * p
-    logp, grad = f(theta)
-    p = p + 0.5 * dt * grad
-
-    return theta, p, logp, grad
-
-
-def compute_hamiltonian(logp, p):
-    return - logp + 0.5 * np.dot(p, p)
-
-
-def draw_momentum(n_param):
-    return np.random.randn(n_param)
