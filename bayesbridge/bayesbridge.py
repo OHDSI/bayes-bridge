6939d79acffc10d0307d8161ccee21c7caf4b366
Aki Nishimura <akihiko4@g.ucla.edu>
2019-03-20 10:57:29 -0700

    Add a check for the edge case without any shrunk coefficients.

--- bayesbridge/bayesbridge.py
+++ bayesbridge/bayesbridge.py
@@ -446,6 +446,9 @@ class BayesBridge():
             self, gshrink, beta_with_shrinkage, shrinkage_exponent, method='sample'):
         # :param method: {"sample", "optimize", None}
 
+        if beta_with_shrinkage.size == 0:
+            return 1. # arbitrary float value as a placeholder
+
         lower_bd = 1 / len(beta_with_shrinkage) / self.compute_power_exp_ave_magnitude(shrinkage_exponent)
             # Solve for $ (expected value of coefficient given global shrinkage) = p^{-1} $.
 
