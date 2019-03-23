5f8379cef88b42a9968aea83f0ad128b383f81eb
Aki Nishimura <akihiko4@g.ucla.edu>
2019-03-22 17:02:24 -0700

    Test centering and intercept through dot operations.

--- tests/test_design_matrix.py
+++ tests/test_design_matrix.py
@@ -7,29 +7,37 @@ atol = 10e-10
 rtol = 10e-10
 
 
-def test_sparse_design_centering():
+def test_sparse_design_intercept_and_centering():
 
     n_obs, n_pred = (100, 10)
     X = simulate_design(n_obs, n_pred, binary_frac=.5, format_='sparse')
-    X_design = SparseDesignMatrix(X, centered=True, add_intercept=False)
-    X_dense = X.toarray()
-    X_dense -= X_dense.mean(axis=0)[np.newaxis, :]
-    w, v = (np.random.randn(size) for size in X.shape)
-    assert np.allclose(X_dense.mean(axis=0), np.zeros(X_dense.shape[1]))
+    X_design = SparseDesignMatrix(X, centered=True, add_intercept=True)
+    X_ndarray = center_and_add_intercept(X.toarray())
+    w, v = (np.random.randn(size) for size in X_design.shape)
     assert np.allclose(
-        X_design.dot(v), X_dense.dot(v), atol=atol, rtol=rtol
+        X_design.dot(v), X_ndarray.dot(v), atol=atol, rtol=rtol
     )
     assert np.allclose(
-        X_design.Tdot(w), X_dense.T.dot(w), atol=atol, rtol=rtol
+        X_design.Tdot(w), X_ndarray.T.dot(w), atol=atol, rtol=rtol
     )
 
 
-def test_dense_design_centering():
+def test_dense_design_intercept_and_centering():
     n_obs, n_pred = (100, 10)
     X = simulate_design(n_obs, n_pred, binary_frac=.5, format_='dense')
-    X_design = DenseDesignMatrix(X, centered=True, add_intercept=False)
-    X_centered = X_design.toarray()
+    X_design = DenseDesignMatrix(X, centered=True, add_intercept=True)
+    X_ndarray = center_and_add_intercept(X)
+    w, v = (np.random.randn(size) for size in X_design.shape)
     assert np.allclose(
-        X_centered.mean(axis=0), np.zeros(X_centered.shape[1]),
-        atol=atol, rtol=rtol
+        X_design.dot(v), X_ndarray.dot(v), atol=atol, rtol=rtol
     )
+    assert np.allclose(
+        X_design.Tdot(w), X_ndarray.T.dot(w), atol=atol, rtol=rtol
+    )
+
+
+def center_and_add_intercept(X):
+    X -= X.mean(axis=0)[np.newaxis, :]
+    intercept_column = np.ones((X.shape[0], 1))
+    X = np.hstack((intercept_column, X))
+    return X
