904c33df189a6ff918e4130db71fb14713e889b3
Aki Nishimura <akihiko4@g.ucla.edu>
2019-10-25 13:05:07 -0700

    Separate out a utility function from a test module.

---
+++ tests/helper.py
@@ -0,0 +1,39 @@
+import numpy as np
+import scipy as sp
+from bayesbridge.model import LinearModel, LogisticModel, CoxModel
+from bayesbridge.design_matrix import SparseDesignMatrix, DenseDesignMatrix
+from simulate_data import simulate_design
+
+
+def simulate_data(model, n_obs=100, n_pred=50, seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+
+    X = simulate_design(n_obs, n_pred, binary_frac=.9)
+
+    beta = np.random.randn(n_pred)
+    n_trial = None
+    if model == 'linear':
+        y = LinearModel.simulate_outcome(X, beta, noise_sd=1.)
+    elif model == 'logit':
+        n_trial = np.random.binomial(np.arange(n_obs) + 1, .5)
+        n_success = LogisticModel.simulate_outcome(n_trial, X, beta)
+        y = (n_trial, n_success)
+    elif model == 'cox':
+        event_time, censoring_time = CoxModel.simulate_outcome(X, beta)
+        event_time, censoring_time, X = \
+            CoxModel._permute_observations_by_event_and_censoring_time(
+                event_time, censoring_time, X)
+        event_time, censoring_time, X = \
+            CoxModel._drop_uninformative_observations(event_time,
+                                                      censoring_time, X)
+        y = (event_time, censoring_time)
+    else:
+        raise NotImplementedError()
+
+    if sp.sparse.issparse(X):
+        X = SparseDesignMatrix(X, add_intercept=False)
+    else:
+        X = DenseDesignMatrix(X, add_intercept=False)
+
+    return y, X, beta
--- tests/test_likelihood_models.py
+++ tests/test_likelihood_models.py
@@ -9,9 +9,8 @@ import scipy.sparse
 from functools import partial
 from .derivative_tester \
     import numerical_grad_is_close, numerical_direc_deriv_is_close
-from simulate_data import simulate_design
+from .helper import simulate_data
 from bayesbridge.model import LinearModel, LogisticModel, CoxModel
-from bayesbridge.design_matrix import SparseDesignMatrix, DenseDesignMatrix
 
 
 def test_linear_model_gradient_and_hessian():
@@ -127,36 +126,3 @@ def test_cox_model_hessian_matvec():
     f = cox_model.compute_loglik_and_gradient
     hessian_matvec = cox_model.get_hessian_matvec_operator(beta)
     assert numerical_direc_deriv_is_close(f, beta, hessian_matvec, seed=0)
-
-
-def simulate_data(model, n_obs=100, n_pred=50, seed=None):
-
-    if seed is not None:
-        np.random.seed(seed)
-        
-    X = simulate_design(n_obs, n_pred, binary_frac=.9)
-
-    beta = np.random.randn(n_pred)
-    n_trial = None
-    if model == 'linear':
-        y = LinearModel.simulate_outcome(X, beta, noise_sd=1.)
-    elif model == 'logit':
-        n_trial = np.random.binomial(np.arange(n_obs) + 1, .5)
-        n_success = LogisticModel.simulate_outcome(n_trial, X, beta)
-        y = (n_trial, n_success)
-    elif model == 'cox':
-        event_time, censoring_time = CoxModel.simulate_outcome(X, beta)
-        event_time, censoring_time, X = \
-            CoxModel._permute_observations_by_event_and_censoring_time(event_time, censoring_time, X)
-        event_time, censoring_time, X = \
-            CoxModel._drop_uninformative_observations(event_time, censoring_time, X)
-        y = (event_time, censoring_time)
-    else:
-        raise NotImplementedError()
-
-    if sp.sparse.issparse(X):
-        X = SparseDesignMatrix(X, add_intercept=False)
-    else:
-        X = DenseDesignMatrix(X, add_intercept=False)
-
-    return y, X, beta
