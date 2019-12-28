974c9db61d389c53e5d87169190c38ddde6e8bde
Aki Nishimura <akihiko4@g.ucla.edu>
2019-12-27 16:09:34 -0800

    Check and remove the redundant calculation

--- bayesbridge/reg_coef_sampler/reg_coef_sampler.py
+++ bayesbridge/reg_coef_sampler/reg_coef_sampler.py
@@ -73,10 +73,6 @@ class SparseRegressionCoefficientSampler():
 
         v = X.Tdot(obs_prec * y)
         prior_shrunk_scale = self.compute_prior_shrunk_scale(gscale, lscale)
-        prior_shrunk_scale = gscale * lscale
-        prior_shrunk_scale /= np.sqrt(
-            1 + (prior_shrunk_scale / self.regularizing_slab_size) ** 2
-        )   # Account for the regularization.
         prior_sd = np.concatenate((
             self.prior_sd_for_unshrunk, prior_shrunk_scale
         ))
