9f402af7985ee3482e39db07b4f4eb5d91a66629
Aki Nishimura <akihiko4@g.ucla.edu>
2020-05-25 17:10:28 -0700

    Replace 'type' with 'family' for model specifier variable name
    'type' has overloaded meanings in programing and 'family' is used in R

--- bayesbridge/model/factory.py
+++ bayesbridge/model/factory.py
@@ -7,20 +7,20 @@ from .cox_model import CoxModel
 from ..design_matrix import DenseDesignMatrix, SparseDesignMatrix
 
 def RegressionModel(
-        outcome, X, model_type='linear',
+        outcome, X, family='linear',
         add_intercept=None, center_predictor=False
     ):
     """
     Params
     ------
-    outcome : vector if model_type == 'linear' else tuple
-        (n_success, n_trial) if model_type == 'logistic'.
+    outcome : vector if family == 'linear' else tuple
+        (n_success, n_trial) if family == 'logistic'.
             The outcome is assumed binary if n_trial is None.
-        (event_time, censoring_time) if model_type == 'cox'
+        (event_time, censoring_time) if family == 'cox'
     X : numpy array or scipy sparse matrix
     n_trial : vector
-        Used for the logistic model_type for binomial outcomes.
-    model_type : str, {'linear', 'logit'}
+        Used for the logistic family for binomial outcomes.
+    family : str, {'linear', 'logit'}
     """
 
     # TODO: Make each MCMC run more "independent" i.e. not rely on the
@@ -28,9 +28,9 @@ def RegressionModel(
     # sampler probably depends too much the stuffs here.
 
     if add_intercept is None:
-        add_intercept = (model_type != 'cox')
+        add_intercept = (family != 'cox')
 
-    if model_type == 'cox':
+    if family == 'cox':
         if add_intercept:
             add_intercept = False
             warn("Intercept is not identifiable in Cox model and won't be added.")
@@ -45,12 +45,12 @@ def RegressionModel(
         X, add_intercept=add_intercept, center_predictor=center_predictor
     )
 
-    if model_type == 'linear':
+    if family == 'linear':
         model = LinearModel(outcome, design)
-    elif model_type == 'logit':
+    elif family == 'logit':
         n_success, n_trial = outcome
         model = LogisticModel(n_success, n_trial, design)
-    elif model_type == 'cox':
+    elif family == 'cox':
         model = CoxModel(event_time, censoring_time, design)
     else:
         raise NotImplementedError()
