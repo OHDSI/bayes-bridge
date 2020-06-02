9dbba04ca2b2404d7fc1489b9d2ec492efbf3b5b
Aki Nishimura <akihiko4@g.ucla.edu>
2020-06-01 17:40:07 -0700

    Test more specific aspect of global scale parametrization

--- tests/test_prior.py
+++ tests/test_prior.py
@@ -9,6 +9,31 @@ from bayesbridge import BayesBridge, RegressionModel, RegressionCoefPrior
 
 
 def test_gscale_parametrization():
+    """ Check that the Gamma hyper-parameters do not depend on parametrization. """
+
+    gscale_hyper_param = {'log10_mean': - 4., 'log10_sd': 1.}
+    bridge_exp = .25
+
+    prior_coef_scale = RegressionCoefPrior(
+        bridge_exponent=bridge_exp,
+        global_scale_prior_hyper_param=gscale_hyper_param,
+        global_scale_parametrization='regress_coef'
+    )
+
+    unit_bridge_magnitude \
+        = RegressionCoefPrior.compute_power_exp_ave_magnitude(bridge_exp)
+    gscale_hyper_param['log10_mean'] -= np.log10(unit_bridge_magnitude)
+    prior_raw_scale = RegressionCoefPrior(
+        bridge_exponent=bridge_exp,
+        global_scale_prior_hyper_param=gscale_hyper_param,
+        global_scale_parametrization='raw'
+    )
+    assert (
+        prior_coef_scale.param['gscale_neg_power'] == prior_raw_scale.param['gscale_neg_power']
+    )
+
+
+def test_gscale_paramet_invariance():
     """ Check sampler outputs are invariant under global scale parametrization. """
 
     y, X, beta = simulate_data(model='logit', seed=0)
