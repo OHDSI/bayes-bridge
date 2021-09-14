8f758e9ca4e8ad874100d6f6d54e9af4535893f5
chinandrew <chinandrew96@gmail.com>
2021-09-14 12:46:02 -0400

    gi_bleed benchmark

--- gi_bleed.py
+++ gi_bleed.py
@@ -1,28 +1,15 @@
 import numpy as np
 from bayesbridge import BayesBridge, RegressionModel, RegressionCoefPrior
-from simulate_data import simulate_design, simulate_outcome
 
-n_obs, n_pred = 10 ** 4, 10 ** 3
+import util.data_manager as data_manager
 
-X = simulate_design(
-    n_obs, n_pred,
-    binary_frac=.9,
-    binary_pred_freq=.2,
-    shuffle_columns=True,
-    format_='sparse',
-    seed=111
+dm = data_manager.DataManager(
+    path_to_data='../gi_bleed/',
+    regress_on='treatment'
 )
 
-beta_true = np.zeros(n_pred)
-beta_true[:5] = 1.5
-beta_true[5:10] = 1.
-beta_true[10:15] = .5
-
-n_trial = np.ones(X.shape[0]) # Binary outcome.
-y = simulate_outcome(
-    X, beta_true, intercept=0., 
-    n_trial=n_trial, model='logit', seed=1
-)
+y, X = dm.read_ohdsi_data()
+y = np.squeeze(y.toarray())
 
 model = RegressionModel(
     y, X, family='logit',
@@ -31,7 +18,7 @@ model = RegressionModel(
 
 prior = RegressionCoefPrior(
     bridge_exponent=.5,
-    n_fixed_effect=0, 
+    n_fixed_effect=0,
         # Number of coefficients with Gaussian priors of pre-specified sd.
     sd_for_intercept=float('inf'),
         # Set it to float('inf') for a flat prior.
