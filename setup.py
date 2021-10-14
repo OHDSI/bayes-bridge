acd775f2472d7e7c5fdea870dd10ffb1dc000dba
Aki Nishimura <aki.nishimura@jhu.edu>
2021-10-14 14:44:41 -0400

    Bump up micro ver to reflect interface and design change

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ ext_modules = [
 
 setup(
     name='bayesbridge',
-    version='0.2.4',
+    version='0.2.5',
     description=\
         'Generates posterior samples under Bayesian sparse regression based on '
         + 'the bridge prior using the CG-accelerated Gibbs sampler of Nishimura '
