27cf4baf19010ff4146fc87e15a84fdcf2dbaec8
Aki Nishimura <aki.nishimura@jhu.edu>
2024-02-18 13:37:41 -0500

    Refactor cholesky-based reg coef sampler

--- bayesbridge/reg_coef_sampler/direct_gaussian_sampler.py
+++ bayesbridge/reg_coef_sampler/direct_gaussian_sampler.py
@@ -1,12 +1,12 @@
 import numpy as np
 import scipy as sp
-import scipy.sparse
 
-def generate_gaussian_with_weight(X, obs_prec, prior_prec_sqrt, z, rand_gen=None):
+def generate_gaussian_with_weight(design, obs_prec, prior_prec_sqrt, z, rand_gen=None):
     """
-    Generate a multi-variate Gaussian with the mean mu and covariance Sigma of the form
-        mu = Sigma z,
-        Sigma^{-1} = X' diag(obs_prec) X + diag(prior_prec_sqrt) ** 2,
+    Generate a multi-variate Gaussian with
+        mean = Sigma z,
+        covariance^{-1} = X' diag(obs_prec) X + diag(prior_prec_sqrt) ** 2,
+    where X is the `design` matrix.
 
     Parameters
     ----------
@@ -14,29 +14,31 @@ def generate_gaussian_with_weight(X, obs_prec, prior_prec_sqrt, z, rand_gen=None
         prior_prec_sqrt : 1-d numpy array
     """
 
-    diag_sqrt = prior_prec_sqrt.copy()
-    fisher_info = X.compute_fisher_info(weight=obs_prec, diag_only=True)
-    fisher_info[fisher_info < 0.] = 0.
-    fisher_info_sqrt = np.sqrt(fisher_info)
-    has_pos_prior_prec = (prior_prec_sqrt > 0)
-    has_zero_prior_prec = np.logical_not(has_pos_prior_prec)
-    diag_sqrt[has_pos_prior_prec] *= np.sqrt(
-        1. + (fisher_info_sqrt[has_pos_prior_prec] / diag_sqrt[has_pos_prior_prec]) ** 2
+    Prec_precond, precond_scale \
+        = compute_jacobi_precond_post_prec(design, obs_prec, prior_prec_sqrt)
+    Prec_precond_chol = sp.linalg.cholesky(Prec_precond, precond_scale)
+    mean_precond = sp.linalg.cho_solve(
+        (Prec_precond_chol, False), precond_scale * z
     )
-    diag_sqrt[has_zero_prior_prec] = fisher_info_sqrt[has_zero_prior_prec]
-    inv_sqrt_diag_scale = 1 / diag_sqrt
-    Phi_scaled = inv_sqrt_diag_scale[:, np.newaxis] \
-        * X.compute_fisher_info(obs_prec) * inv_sqrt_diag_scale[np.newaxis, :]
-    Phi_scaled += np.diag((inv_sqrt_diag_scale * prior_prec_sqrt) ** 2)
-    Phi_scaled_chol = sp.linalg.cholesky(Phi_scaled)
-    mu = sp.linalg.cho_solve((Phi_scaled_chol, False), inv_sqrt_diag_scale * z)
     if rand_gen is None:
-        gaussian_vec = np.random.randn(X.shape[1])
+        gaussian_vec = np.random.randn(design.shape[1])
     else:
-        gaussian_vec = rand_gen.np_random.randn(X.shape[1])
-    beta_scaled = mu + sp.linalg.solve_triangular(
-        Phi_scaled_chol, gaussian_vec, lower=False
+        gaussian_vec = rand_gen.np_random.randn(design.shape[1])
+    sample_precond = mean_precond
+    sample_precond += sp.linalg.solve_triangular(
+        Prec_precond_chol, gaussian_vec, lower=False
     )
-    beta = inv_sqrt_diag_scale * beta_scaled
+    sample = precond_scale * sample_precond
 
-    return beta
+    return sample
+
+def compute_jacobi_precond_post_prec(design, obs_prec, prior_prec_sqrt):
+    diag = prior_prec_sqrt ** 2 \
+        + design.compute_fisher_info(weight=obs_prec, diag_only=True)
+    precond_scale = 1 / np.sqrt(diag)
+    Prec_precond = \
+        precond_scale[:, np.newaxis] \
+        * design.compute_fisher_info(obs_prec) \
+        * precond_scale[np.newaxis, :]
+    Prec_precond += np.diag((precond_scale * prior_prec_sqrt) ** 2)
+    return Prec_precond, precond_scale
