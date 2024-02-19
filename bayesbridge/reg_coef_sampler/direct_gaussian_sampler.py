3c79452ee0ff78ceee102e574705eed14b2d218a
Aki Nishimura <aki.nishimura@jhu.edu>
2024-02-19 10:43:11 -0500

    Refactor slightly to make a role of func more precise

--- bayesbridge/reg_coef_sampler/direct_gaussian_sampler.py
+++ bayesbridge/reg_coef_sampler/direct_gaussian_sampler.py
@@ -14,11 +14,15 @@ def generate_gaussian_with_weight(design, obs_prec, prior_prec_sqrt, z, rand_gen
         prior_prec_sqrt : 1-d numpy array
     """
 
-    Prec_precond, precond_scale \
-        = compute_jacobi_precond_post_prec(design, obs_prec, prior_prec_sqrt)
-    Prec_precond_chol = sp.linalg.cholesky(Prec_precond, precond_scale)
+    diag = prior_prec_sqrt ** 2 \
+           + design.compute_fisher_info(weight=obs_prec, diag_only=True)
+    jacobi_precond_scale = 1 / np.sqrt(diag)
+    Prec_precond = compute_precond_post_prec(
+        design, obs_prec, prior_prec_sqrt, jacobi_precond_scale
+    )
+    Prec_precond_chol = sp.linalg.cholesky(Prec_precond, jacobi_precond_scale)
     mean_precond = sp.linalg.cho_solve(
-        (Prec_precond_chol, False), precond_scale * z
+        (Prec_precond_chol, False), jacobi_precond_scale * z
     )
     if rand_gen is None:
         gaussian_vec = np.random.randn(design.shape[1])
@@ -28,17 +32,14 @@ def generate_gaussian_with_weight(design, obs_prec, prior_prec_sqrt, z, rand_gen
     sample_precond += sp.linalg.solve_triangular(
         Prec_precond_chol, gaussian_vec, lower=False
     )
-    sample = precond_scale * sample_precond
+    sample = jacobi_precond_scale * sample_precond
 
     return sample
 
-def compute_jacobi_precond_post_prec(design, obs_prec, prior_prec_sqrt):
-    diag = prior_prec_sqrt ** 2 \
-        + design.compute_fisher_info(weight=obs_prec, diag_only=True)
-    precond_scale = 1 / np.sqrt(diag)
+def compute_precond_post_prec(design, obs_prec, prior_prec_sqrt, precond_scale):
     Prec_precond = \
         precond_scale[:, np.newaxis] \
         * design.compute_fisher_info(obs_prec) \
         * precond_scale[np.newaxis, :]
     Prec_precond += np.diag((precond_scale * prior_prec_sqrt) ** 2)
-    return Prec_precond, precond_scale
+    return Prec_precond
