9f46778e43b5043559b261ce5fca7d18d8b6f1b2
graced03 <dxy_03@outlook.com>
2024-02-21 16:40:59 -0500

    update the horseshoe prior to include the skew version, update the helper functions to sample from a list of a and c

--- bayesbridge/prior/horseshoe.py
+++ bayesbridge/prior/horseshoe.py
@@ -2,7 +2,8 @@ import math
 from warnings import warn
 import numpy as np
 import scipy as sp
-from ..random import sample_horseshoe_local_scale
+from ..random.local_scale_sampler import sample_horseshoe_local_scale
+from .transfer_learning_helper import compute_horseshoe_lscale
 
 class HorseshoePrior():
 
@@ -15,7 +16,7 @@ class HorseshoePrior():
             skew_mean=0.,
             skew_sd=1.,
             global_scale_prior=None
-        ):
+    ):
         """ Encapisulate horseshoe prior information for BayesBridge.
 
         Parameters
@@ -59,24 +60,29 @@ class HorseshoePrior():
         self.n_fixed = n_fixed_effect
         self.gscale_prior = global_scale_prior
         self.name = "horseshoe"
+        self.skew_mean = skew_mean
+        self.skew_sd = skew_sd
         self.bridge_exp = None
         self._gscale_paramet = None
         if self.gscale_prior is None:
             self.param = {
                 'gscale_neg_power': {'shape': 0., 'rate': 0.},
-                    # Reference prior for a scale family.
+                # Reference prior for a scale family.
                 'gscale': None
             }
 
-
-    def update_local_scale(self, gscale, coef, rg):
+    def update_local_scale(self, gscale, coef, beta_mean_prior, beta_sd_prior, rg):
         # rg: random generator
         #     we might want to switch to the rg for sampling
 
         # TODO: Implemement, probably starting with the unskewed version.
         # the unskewed version uses the bridge prior
-        # TODO: write a function to find all the coef without prior info
-        lscale = sample_horseshoe_local_scale(coef, gscale)
+        # lscale = sample_horseshoe_local_scale(coef, gscale)
+
+        # switch for the function that works for both skewed and centered version
+        lscale, acc_count = compute_horseshoe_lscale(coef, gscale, self.skew_mean, self.skew_sd)
+
+        # ar = 1 / np.mean(acc_count)
 
         # TODO: Pick the lower and upper bound more carefully.
         if np.any(lscale == 0):
--- bayesbridge/prior/transfer_learning_helper.py
+++ bayesbridge/prior/transfer_learning_helper.py
@@ -2,28 +2,30 @@
 #  one option is to store them in a csv file,
 #  then for each beta, you find whether it's informed or not
 import numpy as np
+from ..random.local_scale_sampler import skewed_shrinkage_rejection_sampler
 
 
-def compute_horseshoe_lscale():
-    if no_prior_info:
-        lscale = sample_horseshoe_local_scale(coef, gscale)
-    # TODO: Implemement the skewed version
-    # the skewed version uses the horseshoe prior, with information from larger/another database
+def compute_horseshoe_lscale(beta_coef,
+                             gscale,
+                             skew_prior_mean,
+                             skew_prior_sd,
+                             q=0.5, k1=16, k2=5):
+    a = beta_coef / np.sqrt(2) * gscale * skew_prior_mean
+    c = gscale * skew_prior_sd / beta_coef
 
-    # TODO: should be another function
-    #  for each beta coefficient with prior information,
-    #  do the following updates on the local scale param
-    else:
-        beta_mean_prior, beta_var_prior = get_informed_prior(mu_beta_A, var_beta_a, rho, sigma_sq_ave, sigma_sq_db)
-        a = beta_coef / np.sqrt(2) * gscale * beta_sd_prior
-        c = gscale_ * beta_mean_prior / beta_coef
-        lscale = 1 / skewed_shrinkage_rejection_sampler(a, c, q=0.5, k1=16, k2=5)
-    pass
+    a_c_list = list(zip(a,c))
+    lscale_inv_ar_list = [
+        skewed_shrinkage_rejection_sampler(a_c[0], a_c[1], q=q, k1=k1, k2=k2)
+        for a_c in a_c_list
+    ]
+    lscale_inv_arr = np.array([i[0] for i in lscale_inv_ar_list])
+    acc_count = np.array([i[1] for i in lscale_inv_ar_list])
+    return 1 / lscale_inv_arr, acc_count
 
 
-def get_informed_prior(mu_beta_a, var_beta_a, r, dist, sigma_sq_ave, sigma_sq_db):
+def get_informed_prior(mu_beta_a, r, dist, sigma_sq_ave, sigma_sq_db):
     sigma_sq_coef = sigma_sq_ave + sigma_sq_db
     rho = np.exp(- r * dist)
     gamma = rho * sigma_sq_ave / sigma_sq_coef
     gamma_sq = gamma ** 2
-    return gamma * mu_beta_a, (1 - gamma_sq) * sigma_sq_coef + gamma_sq * var_beta_a
+    return gamma * mu_beta_a, np.sqrt((1 - gamma_sq) * sigma_sq_coef)
